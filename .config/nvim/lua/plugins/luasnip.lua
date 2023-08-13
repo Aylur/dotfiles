a916223f9518e8f205822b355b3b0cb134f5942c
Aylur <demstof@gmail.com>
2023-08-13 02:53:13 +0200

    nvim: remove luasnip tab keymap

--- .config/nvim/lua/plugins/luasnip.lua
+++ .config/nvim/lua/plugins/luasnip.lua
@@ -25,30 +25,30 @@ return {
             local luasnip = require("luasnip")
             local cmp = require("cmp")
 
-            opts.mapping = vim.tbl_extend("force", opts.mapping, {
-                ["<Tab>"] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_next_item()
-                    -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-                    -- this way you will only jump inside the snippet region
-                    elseif luasnip.expand_or_jumpable() then
-                        luasnip.expand_or_jump()
-                    elseif has_words_before() then
-                        cmp.complete()
-                    else
-                        fallback()
-                    end
-                end, { "i", "s" }),
-                ["<S-Tab>"] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_prev_item()
-                    elseif luasnip.jumpable(-1) then
-                        luasnip.jump(-1)
-                    else
-                        fallback()
-                    end
-                end, { "i", "s" }),
-            })
+            -- opts.mapping = vim.tbl_extend("force", opts.mapping, {
+            --     ["<Tab>"] = cmp.mapping(function(fallback)
+            --         if cmp.visible() then
+            --             cmp.select_next_item()
+            --         -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+            --         -- this way you will only jump inside the snippet region
+            --         elseif luasnip.expand_or_jumpable() then
+            --             luasnip.expand_or_jump()
+            --         elseif has_words_before() then
+            --             cmp.complete()
+            --         else
+            --             fallback()
+            --         end
+            --     end, { "i", "s" }),
+            --     ["<S-Tab>"] = cmp.mapping(function(fallback)
+            --         if cmp.visible() then
+            --             cmp.select_prev_item()
+            --         elseif luasnip.jumpable(-1) then
+            --             luasnip.jump(-1)
+            --         else
+            --             fallback()
+            --         end
+            --     end, { "i", "s" }),
+            -- })
         end,
     },
 }
