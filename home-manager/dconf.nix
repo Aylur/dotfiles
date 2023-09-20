743326313e8dd280c47f59f8e847b9f67235673d
Aylur <demstof@gmail.com>
2023-09-21 01:05:29 +0200

    generate dconf.nix

--- home-manager/dconf.nix
+++ home-manager/dconf.nix
@@ -1,5 +1,97 @@
-{
+{ lib, ... }: with lib.hm.gvariant; {
   dconf.settings = {
+    "org/gnome/desktop/input-sources" = {
+      sources = [ (mkTuple [ "xkb" "hu" ]) ];
+      xkb-options = [ "terminate:ctrl_alt_bksp" ];
+    };
+
+    "org/gnome/desktop/interface" = {
+      show-battery-percentage = true;
+    };
+
+    "org/gnome/desktop/peripherals/touchpad" = {
+      tap-to-click = true;
+      two-finger-scrolling-enabled = true;
+    };
+
+    "org/gnome/desktop/search-providers" = {
+      disabled = [ "org.gnome.Boxes.desktop" ];
+      enabled = [ "org.gnome.Weather.desktop" ];
+      sort-order = [
+        "org.gnome.Contacts.desktop"
+        "org.gnome.Documents.desktop"
+        "org.gnome.Nautilus.desktop"
+        "org.gnome.Calendar.desktop"
+        "org.gnome.Calculator.desktop"
+        "org.gnome.Software.desktop"
+        "org.gnome.Settings.desktop"
+        "org.gnome.clocks.desktop"
+        "org.gnome.design.IconLibrary.desktop"
+        "org.gnome.seahorse.Application.desktop"
+        "org.gnome.Weather.desktop"
+        "org.gnome.Boxes.desktop"
+      ];
+    };
+
+    "org/gnome/desktop/session" = {
+      idle-delay = mkUint32 0;
+    };
+
+    "org/gnome/desktop/wm/keybindings" = {
+      close = [ "<Alt>q" ];
+      move-to-workspace-1 = [ "<Shift><Super>1" ];
+      move-to-workspace-2 = [ "<Shift><Super>2" ];
+      move-to-workspace-3 = [ "<Shift><Super>3" ];
+      move-to-workspace-4 = [ "<Shift><Super>4" ];
+      move-to-workspace-5 = [ "<Shift><Super>5" ];
+      switch-to-workspace-1 = [ "<Super>1" ];
+      switch-to-workspace-2 = [ "<Super>2" ];
+      switch-to-workspace-3 = [ "<Super>3" ];
+      switch-to-workspace-4 = [ "<Super>4" ];
+      switch-to-workspace-5 = [ "<Super>5" ];
+      toggle-fullscreen = [ "<Super>g" ];
+    };
+
+    "org/gnome/desktop/wm/preferences" = {
+      mouse-button-modifier = "<Super>";
+      num-workspaces = 5;
+      resize-with-right-button = true;
+    };
+
+    "org/gnome/mutter" = {
+      dynamic-workspaces = false;
+      edge-tiling = true;
+      num-workspaces = 5;
+      workspaces-only-on-primary = true;
+    };
+
+    "org/gnome/settings-daemon/plugins/media-keys" = {
+      custom-keybindings = [ "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/" ];
+      mic-mute = [ "AudioMicMute" ];
+      next = [ "AudioNext" ];
+      play = [ "AudioPlay" ];
+      previous = [ "AudioPrev" ];
+      stop = [ "AudioStop" ];
+      volume-down = [ "AudioLowerVolume" ];
+      volume-up = [ "AudioRaiseVolume" ];
+
+      home = [ "<Super>e" ];
+      www = [ "<Super>w" ];
+    };
+
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+      binding = "<Super>Return";
+      command = "wez";
+      name = "term";
+    };
+
+    "org/gnome/settings-daemon/plugins/power" = {
+      idle-dim = false;
+      power-button-action = "interactive";
+      sleep-inactive-ac-type = "nothing";
+      sleep-inactive-battery-type = "nothing";
+    };
+
     "org/gnome/shell" = {
       favorite-apps = [
         "firefox.desktop"
@@ -17,57 +109,16 @@
       ];
     };
 
+    "org/gnome/shell/app-switcher" = {
+      current-workspace-only = false;
+    };
+
+    "org/gnome/shell/keybindings" = {
+      toggle-application-view = [ "<Super>r" ];
+    };
+
     "system/locale" = {
       region = "hu_HU.UTF-8";
-    };
-
-    "org/gnome/desktop/wm/keybindings" = {
-      close = ["<Alt>q"];
-      toggle-fullscreen =  ["<Super>g"];
-      switch-to-workspace-1 = ["<Super>1"];
-      switch-to-workspace-2 = ["<Super>2"];
-      switch-to-workspace-3 = ["<Super>3"];
-      switch-to-workspace-4 = ["<Super>4"];
-      switch-to-workspace-5 = ["<Super>5"];
-      move-to-workspace-1 = ["<Shift><Super>1"];
-      move-to-workspace-2 = ["<Shift><Super>2"];
-      move-to-workspace-3 = ["<Shift><Super>3"];
-      move-to-workspace-4 = ["<Shift><Super>4"];
-      move-to-workspace-5 = ["<Shift><Super>5"];
-    };
-
-    "org/gnome/settings-daemon/plugins/media-keys"= {
-      mic-mute = ["AudioMicMute"];
-      next = ["AudioNext"];
-      pause = ["AudioStop"];
-      play = ["AudioPlay"];
-      previous = ["AudioPrev"];
-      volume-down = ["AudioLowerVolume"];
-      volume-up = ["AudioRaiseVolume"];
-
-      home = ["<Super>e"];
-      www = ["<Super>w"];
-    };
-
-    "org/gnome/desktop/peripherals/touchpad" = {
-      tap-to-click = true;
-    };
-
-    "org/gnome/desktop/wm/preferences" = {
-      resize-with-right-button = true;
-      mouse-button-modifier = "<Super>";
-    };
-
-    "org/gnome/mutter" = {
-      edge-tiling = true;
-      dynamic-workspaces = false;
-      num-workspaces = 5;
-      workspaces-only-on-primary = true;
-    };
-
-    "org/virt-manager/virt-manager/connections" = {
-      autoconnect = ["qemu:///system"];
-      uris = ["qemu:///system"];
     };
   };
 }
