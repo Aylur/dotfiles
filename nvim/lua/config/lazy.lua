b5d350fcfb1d678c6357a672b46c252829993976
Aylur <demstof@gmail.com>
2023-10-17 16:14:50 +0200

    nvim: add formatting.black

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -19,6 +19,7 @@ require("lazy").setup({
         { import = "lazyvim.plugins.extras.lang.clangd" },
         { import = "lazyvim.plugins.extras.lang.go" },
         { import = "lazyvim.plugins.extras.lang.json" },
+        { import = "lazyvim.plugins.extras.formatting.black" },
         { import = "lazyvim.plugins.extras.lang.python" },
         { import = "plugins" },
     },
