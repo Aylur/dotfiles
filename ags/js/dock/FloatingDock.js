3f9ab7cc62c8f40ed70187f14ffbe31240303c1d
Aylur <demstof@gmail.com>
2023-12-05 00:41:37 +0100

    ags: improve dock hovering

--- ags/js/dock/FloatingDock.js
+++ ags/js/dock/FloatingDock.js
@@ -1,13 +1,10 @@
 import Widget from 'resource:///com/github/Aylur/ags/widget.js';
-import * as Utils from 'resource:///com/github/Aylur/ags/utils.js';
 import Hyprland from 'resource:///com/github/Aylur/ags/service/hyprland.js';
 import Dock from './Dock.js';
 import options from '../options.js';
 
 /** @param {number} monitor */
 export default monitor => {
-    let revealed = false;
-
     const revealer = Widget.Revealer({
         transition: 'slide_up',
         child: Dock(),
@@ -23,40 +20,28 @@ export default monitor => {
         },
     });
 
-    const box = Widget.Box({
-        vertical: true,
-        css: 'padding: 1px;',
-        children: [
-            revealer,
-            Widget.Box({
-                class_name: 'padding',
-                css: 'padding: 2px;',
-            }),
-        ],
-    });
-
-    const eventbox = Widget.EventBox({
-        child: box,
-        vpack: 'start',
-        on_hover: () => {
-            Utils.timeout(300, () => revealed = true);
-            revealer.reveal_child = true;
-        },
-        on_hover_lost: () => {
-            if (!revealed)
-                return;
-
-            Utils.timeout(300, () => revealed = false);
-            revealer.reveal_child = false;
-        },
-    });
-
     return Widget.Window({
         monitor,
         name: `dock${monitor}`,
         class_name: 'floating-dock',
         anchor: ['bottom'],
-        child: eventbox,
+        child: Widget.Box({
+            children: [
+                revealer,
+                Widget.Box({
+                    class_name: 'padding',
+                    css: 'padding: 2px;',
+                }),
+            ],
+        }),
+        connections: [
+            ['enter-notify-event', () => {
+                revealer.reveal_child = true;
+            }],
+            ['leave-notify-event', () => {
+                revealer.reveal_child = false;
+            }],
+        ],
         binds: [['visible', options.bar.position, 'value', v => v !== 'bottom']],
     });
 };
