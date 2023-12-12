7605dde586d99f20af7bb8a9424c594eae0ac960
ben <bdebiase2@gmail.com>
2023-12-12 13:24:28 -0500

    ags: prevent dock from hiding in certain situations

--- ags/js/dock/FloatingDock.js
+++ ags/js/dock/FloatingDock.js
@@ -2,6 +2,7 @@ import Widget from 'resource:///com/github/Aylur/ags/widget.js';
 import Hyprland from 'resource:///com/github/Aylur/ags/service/hyprland.js';
 import Dock from './Dock.js';
 import options from '../options.js';
+import Gdk from 'gi://Gdk';
 
 /** @param {number} monitor */
 export default monitor => {
@@ -9,10 +10,12 @@ export default monitor => {
         transition: 'slide_up',
         child: Dock(),
         setup: self => {
+            self.is_hovered = false;
+
             const update = () => {
                 const ws = Hyprland.getWorkspace(Hyprland.active.workspace.id);
                 if (Hyprland.getMonitor(monitor)?.name === ws?.monitor)
-                    self.reveal_child = ws?.windows === 0;
+                    self.reveal_child = self.is_hovered || ws?.windows === 0;
             };
             self.connectTo(Hyprland, update, 'client-added');
             self.connectTo(Hyprland, update, 'client-removed');
@@ -34,14 +37,28 @@ export default monitor => {
                 }),
             ],
         }),
-        connections: [
-            ['enter-notify-event', () => {
-                revealer.reveal_child = true;
-            }],
-            ['leave-notify-event', () => {
-                revealer.reveal_child = false;
-            }],
-        ],
+        setup: self => {
+            self.connections = [
+                ['enter-notify-event', () => {
+                    revealer.is_hovered = true;
+                    revealer.reveal_child = revealer.is_hovered;
+                }],
+                ['leave-notify-event', () => {
+                    const [x, y] = self.get_pointer();
+                    if (revealer.get_realized()) {
+                        const dockRect = self.get_allocation();
+                        const padding = -8;
+                        if (x >= -padding && x <= dockRect.width + padding && y >= -padding && y <= dockRect.height + padding)
+                            return;
+
+                        revealer.is_hovered = false;
+                        const ws = Hyprland.getWorkspace(Hyprland.active.workspace.id);
+                        if (ws?.windows !== 0 && !revealer.is_hovered)
+                            revealer.reveal_child = false;
+                    }
+                }],
+            ]
+        },
         binds: [['visible', options.bar.position, 'value', v => v !== 'bottom']],
     });
 };
