242681688edfcfe587d9f849fa6b43d0050af267
Aylur <demstof@gmail.com>
2023-12-23 23:20:50 +0100

    update: hm and nixos

--- nixos/hyprland.nix
+++ nixos/hyprland.nix
@@ -53,6 +53,7 @@
     devmon.enable = true;
     udisks2.enable = true;
     upower.enable = true;
+    power-profiles-daemon.enable = true;
     accounts-daemon.enable = true;
     gnome = {
       evolution-data-server.enable = true;
