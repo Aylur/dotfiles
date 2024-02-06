8b76f82ec23e2b90fdba1a2d53e6c8e8c144f2e0
Aylur <demstof@gmail.com>
2024-02-06 12:18:07 +0100

    fix: ags setting dialog

--- ags/js/settings/SettingsDialog.js
+++ ags/js/settings/SettingsDialog.js
@@ -237,7 +237,7 @@ const searchEntry = Widget.Revealer({
 
 const categoriesStack = Widget.Stack({
     transition: 'slide_left_right',
-    children: categories.reduce((obj, name) => Object.assign(obj, { name: Page(name) }), {}),
+    children: categories.reduce((obj, name) => { obj[name] = Page(name); return obj }, {}),
     shown: currentPage.bind(),
     visible: search.bind().transform(v => !v),
 });
