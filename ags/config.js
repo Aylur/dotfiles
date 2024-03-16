ffd0a66743affabe0083cb6ac14ce10b7f549039
Aylur <demstof@gmail.com>
2024-03-16 13:19:44 +0100

    ags: esbuild bundler option
    fix #181

--- ags/config.js
+++ ags/config.js
@@ -1,5 +1,8 @@
+import GLib from "gi://GLib"
+
 const main = "/tmp/ags/main.js"
 const entry = `${App.configDir}/main.ts`
+const bundler = GLib.getenv("AGS_BUNDLER") || "bun"
 
 const v = {
     ags: pkg.version.split(".").map(Number),
@@ -7,17 +10,33 @@ const v = {
 }
 
 try {
-    await Utils.execAsync([
-        "bun", "build", entry,
-        "--outfile", main,
-        "--external", "resource://*",
-        "--external", "gi://*",
-        "--external", "file://*",
-    ])
+    switch (bundler) {
+        case "bun": await Utils.execAsync([
+            "bun", "build", entry,
+            "--outfile", main,
+            "--external", "resource://*",
+            "--external", "gi://*",
+            "--external", "file://*",
+        ]); break
+
+        case "esbuild": await Utils.execAsync([
+            "esbuild", "--bundle", entry,
+            "--format=esm",
+            `--outfile=${main}`,
+            "--external:resource://*",
+            "--external:gi://*",
+            "--external:file://*",
+        ]); break
+
+        default:
+            throw `"${bundler}" is not a valid bundler`
+    }
+
     if (v.ags[1] < v.expect[1] || v.ags[2] < v.expect[2]) {
         print(`my config needs at least v${v.expect.join(".")}, yours is v${v.ags.join(".")}`)
         App.quit()
     }
+
     await import(`file://${main}`)
 } catch (error) {
     console.error(error)
