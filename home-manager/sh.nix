19288beee14d2a454b5d08cf8731884bde458395
Aylur <k.demeter@protonmail.com>
2024-11-06 04:39:08 +0100

    fix: nushell env

--- home-manager/sh.nix
+++ home-manager/sh.nix
@@ -61,15 +61,15 @@ in {
       shellAliases = aliases // config.shellAliases;
       enable = true;
       environmentVariables = {
-        PROMPT_INDICATOR_VI_INSERT = "\"  \"";
-        PROMPT_INDICATOR_VI_NORMAL = "\"∙ \"";
-        PROMPT_COMMAND = ''""'';
-        PROMPT_COMMAND_RIGHT = ''""'';
+        PROMPT_INDICATOR_VI_INSERT = "  ";
+        PROMPT_INDICATOR_VI_NORMAL = "∙ ";
+        PROMPT_COMMAND = "";
+        PROMPT_COMMAND_RIGHT = "";
         NIXPKGS_ALLOW_UNFREE = "1";
         NIXPKGS_ALLOW_INSECURE = "1";
-        SHELL = ''"${pkgs.nushell}/bin/nu"'';
-        EDITOR = "'${config.home.sessionVariables.EDITOR}'";
-        VISUAL = "'${config.home.sessionVariables.VISUAL}'";
+        SHELL = "${pkgs.nushell}/bin/nu";
+        EDITOR = config.home.sessionVariables.EDITOR;
+        VISUAL = config.home.sessionVariables.VISUAL;
       };
       extraConfig = let
         conf = builtins.toJSON {
