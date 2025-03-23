0eb9520b06875a2d1c83ebcf1190f16da7480c94
Aylur <k.demeter@protonmail.com>
2025-03-23 14:51:51 +0100

    nvim: explorer on right

--- nvim/lua/plugins/picker.lua
+++ nvim/lua/plugins/picker.lua
@@ -1,3 +1,31 @@
+local function picker()
+	Snacks.picker()
+end
+
+local function explorer()
+	Snacks.explorer({ layout = "right", auto_close = true })
+end
+
+local function file_select()
+	Snacks.picker.smart({ layout = "select" })
+end
+
+local function file_picker()
+	Snacks.picker.smart({ layout = "default" })
+end
+
+local function live_grep()
+	Snacks.picker.smart({ layout = "default" })
+end
+
+local function terminal()
+	Snacks.terminal(vim.env.SHELL)
+end
+
+local function lazygit()
+	Snacks.lazygit()
+end
+
 return {
 	{
 		"folke/snacks.nvim",
@@ -11,15 +39,13 @@ return {
 			lazygit = { configure = true },
 		},
 		keys = {
-            -- stylua: ignore start
-			{ "<leader>pp", function() Snacks.picker() end, desc = "Open Picker" },
-			{ "<leader>e", function() Snacks.explorer({ layout = "select", auto_close = true }) end, desc = "File Explorer" },
-			{ "<leader><space>", function() Snacks.picker.smart({ layout = "select" }) end, desc = "Find Files", },
-			{ "<leader>ff", function() Snacks.picker.smart() end, desc = "Find Files", },
-            { "<leader>fg", function() Snacks.picker.grep() end, desc = "Grep" },
-            { "<leader>ft", function() Snacks.terminal(vim.env.SHELL) end, desc = "Open Terminal" },
-            { "<leader>gg", function() Snacks.lazygit() end, desc = "Git Status" },
-			-- stylua: ignore end
+			{ "<leader>pp", picker, desc = "Open Picker" },
+			{ "<leader>e", explorer, desc = "File Explorer" },
+			{ "<leader><space>", file_select, desc = "Find Files" },
+			{ "<leader>ff", file_picker, desc = "Find Files" },
+			{ "<leader>fg", live_grep, desc = "Grep" },
+			{ "<leader>ft", terminal, desc = "Open Terminal" },
+			{ "<leader>gg", lazygit, desc = "Git Status" },
 		},
 	},
 }
